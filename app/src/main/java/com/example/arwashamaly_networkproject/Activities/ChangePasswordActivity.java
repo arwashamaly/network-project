779775eb0a97d6f092b5ff9a50dfe546ef2e9026
admin <arwashamaly311@gmail.com>
2023-01-14 22:23:52 +0200

    update change password

--- app/src/main/java/com/example/arwashamaly_networkproject/Activities/ChangePasswordActivity.java
+++ app/src/main/java/com/example/arwashamaly_networkproject/Activities/ChangePasswordActivity.java
@@ -1,7 +1,6 @@
 package com.example.arwashamaly_networkproject.Activities;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
@@ -22,21 +21,28 @@ public class ChangePasswordActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         binding = ActivityChangePasswordBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-        binding.btnSave2.setOnClickListener(new View.OnClickListener() {
+
+        binding.btnSaveNewPass.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //updatePassword();
+                String oldPass = binding.etOldPass.getText().toString();
+                String newPass = binding.etNewPass.getText().toString();
+                if (!oldPass.isEmpty() && !newPass.isEmpty()) {
+                    updatePassword(oldPass, newPass);
+                    finish();
+                }
             }
         });
     }
 
-    private void updatePassword(String email, String oldPassword, String newPassword) {
-        AuthCredential credential = EmailAuthProvider.getCredential("", "");
+    private void updatePassword(String oldPassword, String newPassword) {
+        AuthCredential credential = EmailAuthProvider.getCredential(user.getEmail(), oldPassword);
         user.reauthenticate(credential).addOnCompleteListener(new OnCompleteListener<Void>() {
             @Override
             public void onComplete(@NonNull Task<Void> task) {
                 if (task.isSuccessful()) {
-                    user.updatePassword("");
+                    user.updatePassword(newPassword);
+                    Toast.makeText(ChangePasswordActivity.this, "Change password successfully", Toast.LENGTH_SHORT).show();
                 } else {
                     Toast.makeText(ChangePasswordActivity.this, task.getException().getMessage(), Toast.LENGTH_SHORT).show();
                 }
